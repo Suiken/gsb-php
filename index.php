daaf640103edff4d9f7fc4793df55bbd9be52e06
Suiken <kevinsuy@me.com>
2013-02-09 21:01:30 +0100

    Application fonctionnelle

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <section>
+            <fieldset>
+                <legend>hihi</legend>
+                <input type="search"/>
+                <input type="color"/>
+                <input type="mail" />
+            </fieldset>
+        </section>
+    </body>
+</html>
